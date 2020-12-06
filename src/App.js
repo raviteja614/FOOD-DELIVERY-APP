f08828e5385acefdea11afab68d2402d3563fa7c
raviteja614 <56757887+raviteja614@users.noreply.github.com>
2020-12-06 23:11:55 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,51 @@
+
+import './App.css';
+import './login1';
+import {route,link,BrowseRouter as router} from 'react-router-dom';
+import login from './login1'
+import signup from './signup'
+function App() {
+
+  return (
+          
+          <div class="navbar">
+                  <ul>
+                          <li><a>HOME</a></li>
+                          <li><a>ABOUT</a></li>
+                          <li><a>LOGIN</a></li>
+                          <li><a>SIGNUP</a></li>
+                  </ul>
+
+
+         
+    
+      <div className="contents">
+        <h1>ONLINE FOOD DELIVERY</h1>
+        <h3>ORDER YOUR FAVORITES FOOD ONLINE</h3>
+        <p >
+          A Moments of Delivered on Time.<br>
+        </br>
+A life full of Tasty Food.<br>
+        </br>
+Taste that best, its on time.<br>
+        </br>
+Giving your Hunger a new Option.<br>
+        </br>
+A Trendy Food Delivery.<br>
+        </br>
+Love your Hunger.<br>
+        </br>
+Good Food for Good Moments.<br>
+        </br>
+The joy of Getting Best.
+</p>
+      </div>
+      </div>
+
+
+      
+      
+  )
+}
+
+export default App;
